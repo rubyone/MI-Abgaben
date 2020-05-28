f86a2de4c02cc90ba2fa782c10a075ba30e27ced
Ruben Kolsch <ruben54123@hotmail.com>
2020-05-28 20:57:54 +0200

    yes last one

--- src/Abgabe2.java
+++ src/Abgabe2.java
@@ -14,9 +14,13 @@ import at.imagelibrary.StereoImage;
 
 public class Abgabe2 {
     public static void main(String[] args){
-        JFrame f = new JFrame("StereoImage");
+        JFrame f = new JFrame("StereoImage -1");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(408, 325);
+
+        JFrame f1 = new JFrame("StereoImage 1");
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setSize(408, 325);
 
         try {
 
@@ -26,7 +30,20 @@ public class Abgabe2 {
             BufferedImage left = ImageIO.read(new File("src/leftSmall.jpg"));
             BufferedImage right = ImageIO.read(new File("src/rightSmall.jpg"));
 
+
+
             StereoImage stereo = new StereoImage(left, right);
+
+/*
+            public static final int ANAGLYPH_RAD_CYAN_COLOR		=  1;
+            public static final int ANAGLYPH_RAD_CYAN_GRAY		= -1;
+            public static final int ANAGLYPH_RAD_GREEN_COLOR	=  2;
+            public static final int ANAGLYPH_RAD_GREEN_GRAY		= -2;
+            public static final int ANAGLYPH_RAD_BLUE_COLOR		=  3;
+            public static final int ANAGLYPH_RAD_BLUE_GRAY		= -3;
+            public static final int ANAGLYPH_YELLOW_BLUE_COLOR	=  4;
+            public static final int ANAGLYPH_YELLOW_BLUE_GRAY	= -4;
+
 
             double[][] leftFilter = {
                     {1,0,0},
@@ -38,15 +55,19 @@ public class Abgabe2 {
                     {0,0,0},
                     {0,1,0},
                     {0,0,1}
-            };
+            };*/
 
-            ImageIcon icon = new ImageIcon(stereo.getResultImage(leftFilter, rightFilter));
+            ImageIcon icon = new ImageIcon(stereo.getResultImage(-1  ));
+            ImageIcon icon1 = new ImageIcon(stereo.getResultImage(1));
+//            ImageIcon icon = new ImageIcon(stereo.getResultImage(leftFilter, rightFilter));
 
             f.add(new JLabel(icon));
+            f1.add(new JLabel(icon1));
 
             Image img = icon.getImage();
-
-            ImageIO.write((RenderedImage) img, "jpg", new File("src/output.jpg"));
+            Image img1 = icon1.getImage();
+            ImageIO.write((RenderedImage) img1, "jpg", new File("src/1.jpg"));
+            ImageIO.write((RenderedImage) img, "jpg", new File("src/-1.jpg"));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,5 +75,6 @@ public class Abgabe2 {
             e.printStackTrace();
         }
         f.setVisible(true);
+        f1.setVisible(true);
     }
 }
